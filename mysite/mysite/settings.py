53f7809c5fea9f4bc9945eb1df116af3ca6d2ada
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2019-12-18 17:54:34 +0530

    some visual changes

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -124,6 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticroot/')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediaroot/')
+
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
