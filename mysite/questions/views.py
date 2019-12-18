144e2eef759c25f48fae5598cf6e7aced8d40592
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2019-12-18 17:45:07 +0530

    created template and view for answer

--- mysite/questions/views.py
+++ mysite/questions/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Question
+from .models import Question, Answer
 from django.views.generic import ListView, CreateView
-from .forms import QuestionCreateForm
+from .forms import QuestionCreateForm, AnswerCreateForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import get_user_model
 User = get_user_model()
@@ -25,7 +25,16 @@ class QuestionCreateView(LoginRequiredMixin, CreateView):
 def question_detail(request, pk):
     template_name = "questions/detail.html"
     question = get_object_or_404(Question, pk=pk)
+    answers = Answer.objects.filter(answer_for=question)
+    form = AnswerCreateForm(request.POST or None)
+    if form.is_valid():
+        form.instance.answer_for = question
+        form.instance.answered_by = request.user
+        form.save()
+        form = AnswerCreateForm()
     context = {
-        "object": question
+        "object": question,
+        "answers": answers,
+        "form": form
     }
     return render(request, template_name, context)
