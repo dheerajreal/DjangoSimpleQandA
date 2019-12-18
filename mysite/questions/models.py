8816b8f9067dde8c6893cd230776f31aa3167fd5
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2019-12-18 16:34:12 +0530

    added question model

--- mysite/questions/models.py
+++ mysite/questions/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.shortcuts import reverse
+User = get_user_model()
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=128)
+    question_description = models.TextField(null=True, max_length=256)
+    asked_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    asked_datetime = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def __str__(self):
+        return self.question_text
+
+    def get_absolute_url(self):
+        return reverse("question_detail", kwargs={"pk": self.pk})
+
+    class Meta:
+        ordering = ['-asked_datetime']
