507b15ea4bb501be114fdf4ab2f2c4f66c5c0498
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2020-02-02 19:35:56 +0530

    made some visual changes

--- mysite/questions/forms.py
+++ mysite/questions/forms.py
@@ -10,13 +10,13 @@ class QuestionCreateForm(forms.ModelForm):
 
 
 class AnswerCreateForm(forms.ModelForm):
-    answer_text = forms.CharField(
-        widget=forms.Textarea(
-            attrs={
-                "placeholder": "Give an Answer ?", 'cols': 80, 'rows': 4
-            }
-        ),
-    )
+    answer_text = forms.CharField(label="Give an Answer",
+                                  widget=forms.Textarea(
+                                      attrs={
+                                          "placeholder": "", 'cols': 80, 'rows': 4
+                                      }
+                                  ),
+                                  )
 
     class Meta:
         model = Answer
