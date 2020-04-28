a3eaf69776ad9455ce7a945d3af2b1fe9c298fd6
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2020-04-29 03:45:15 +0530

    changed allowed hosts

--- mysite/mysite/variables.py
+++ mysite/mysite/variables.py
@@ -2,7 +2,7 @@ config_vars = {
 
     "SECRET_KEY": "whatever_secret_key",
     "DEBUG": True,
-    "ALLOWED_HOSTS": ["localhost", "0.0.0.0", "127.0.0.1"],
+    "ALLOWED_HOSTS": ["localhost", "0.0.0.0", "127.0.0.1", "*"],
 
 
 }
